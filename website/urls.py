ad422d01d6a828365c9179149d489d10a8e44b00
343taskmaster <140878578+343taskmaster@users.noreply.github.com>
2023-08-25 00:29:39 +0800

    registration1

--- website/urls.py
+++ website/urls.py
@@ -5,9 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     #path('login/', views.login_user, name='login'),
+
     path('logout/', views.logout_user, name='logout'),
-
-
     #logout/ means back a directory 
+
+    path('register/', views.register_user, name='register'),
   
 ]
